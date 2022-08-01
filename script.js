2a68f6ec9b230d777b0818241f22871f71768fa4
KonceptD <kiranjayd@yahoo.com>
2022-08-01 16:29:59 +0200

    HTML: Added a table for displaying each newly added book with buttons to change read status and to delete the book; CSS: Took out most styles to redo when basic functionality is done ; JS: Fixed 'submit' button event listener for adding a new book

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 // * TODO: CHECK FREECODECAMP AS AN ALTERNATE FOR PRACTICE
-// 
+// TODO: New book button that brings up the form from each display
 
 // Grabbed my button to add new book and the div containing the form
 const popUp = document.querySelector("#popUp")
-const popUpbtn = document.querySelector("#new_book_button") 
+// const popUpbtn = document.querySelector("#new_book_button") 
+const saveBtn = document.querySelector("#saveButton")
+// popUp.addEventListener("click", (e) => {
+//     e.style.display = "block"
+// })
+let bookTitle = document.querySelector("#book_title")
+let bookAuthor = document.querySelector("#book_author")
+let bookPages = document.querySelector("#book_pages")
+let bookFinish = document.querySelector("#book_finished")
 
-popUp.addEventListener("click", (e) => {
-    e.style.display = "block"
+saveBtn.addEventListener("click", () => {
+
+    let newBook = bookTitle.value
+    newBook = new Book (bookTitle.value, bookAuthor.value, bookPages.value, bookFinish.value)
+    addToLibrary(newBook);
+    clearForm();
+    displayLibrary();
 })
 
 // My library array
 let myLibrary = [];
 
-// Creating am object with a function in it to display info
+// Creating an object with a function in it to display info
 function Book(title, author, pages, complete) {
     this.title = title
     this.author = author 
@@ -37,17 +50,26 @@ function displayLibrary(){
     });
 }
 
+clearForm = () => {
+
+    bookTitle.value = null 
+    bookAuthor.value = null 
+    bookPages.value = null
+    bookFinish.value = null
+}
+
 // Making new books to populate the library
-const inheritance = new Book("Inheritance", "Christopher Paolini", "601 pages", "Completed")
-const harryPotter = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "280 pages", "Not started")
-const mindset = new Book("Mindset", "Carol Dweck", "400", "Complete")
+// const inheritance = new Book("Inheritance", "Christopher Paolini", "601 pages", "Read")
+// const harryPotter = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "280 pages", "Read")
+// const mindset = new Book("Mindset", "Carol Dweck", "400", "Read")
+
+
 
 // Testing 1 books info method
-console.log(inheritance.info())
 // console.log(inheritance.info());
 
-addToLibrary(inheritance);
-addToLibrary(harryPotter);
-addToLibrary(mindset);
+// addToLibrary(inheritance);
+// addToLibrary(harryPotter);
+// addToLibrary(mindset);
 
-displayLibrary();
+// displayLibrary();
