86a9ebab92ce05f8569de41d23cc2a3b00a4d7ea
KonceptD <kiranjayd@yahoo.com>
2022-08-03 16:26:27 +0200

    HTML: Added 'hidden' class to elements and a return button; CSS: Configured hidden class visibility; JS: Added event listeners for new book and return buttons, added comments to code, refactored code, added a new function,  crossed off an item in the TODOlist

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
 // * TODO: CHECK FREECODECAMP AS AN ALTERNATE FOR PRACTICE
-// TODO: New book button that brings up the form from each display
+// * TODO: New book button that brings up the form from each display - LEARNT: How to toggle elements between hidden and visible using classlist.toggle and the "hidden" attribute
 // TODO: Write each book to table row
 // TODO: Add delete button functionality
 // TODO: Add "Read/Unread" button functionality
 // TODO: CSS - Style for table - Center table
 // TODO: HTML: Column size - maybe 3 width?
 
-// Grabbed my button to add new book and the div containing the form
+// Grabbed new book, save and return buttons, pop up form, and table elements -> variables
 const popUp = document.querySelector("#popUp")
-// const popUpbtn = document.querySelector("#new_book_button") 
 const saveBtn = document.querySelector("#saveButton")
-// popUp.addEventListener("click", (e) => {
-//     e.style.display = "block"
-// })
+const newBookBtn = document.querySelector("#new_book_button")
+const tableDisplay = document.querySelector("table")
+const returnBtn = document.querySelector("#returnButton")
 
-// Assigning each textbox content to a variable
-let bookTitle = document.querySelector("#book_title")
-let bookAuthor = document.querySelector("#book_author")
-let bookPages = document.querySelector("#book_pages")
-let bookFinish = document.querySelector("#book_finished")
+
+
+// Assigning each textbox content of the entered Book to a variable
+let bookTitle = document.querySelector("#book_title").value
+let bookAuthor = document.querySelector("#book_author").value
+let bookPages = document.querySelector("#book_pages").value
+let bookFinish = document.querySelector("#book_finished").value
+
 
 // E.Listener which creates and adds book to library
 saveBtn.addEventListener("click", () => {
      
-    let newBook = bookTitle.value
-    newBook = new Book (bookTitle.value, bookAuthor.value, bookPages.value, bookFinish.value)
+    let newBook = bookTitle
+    newBook = new Book (bookTitle, bookAuthor, bookPages, bookFinish)
     addToLibrary(newBook);
     clearForm();
-
     displayLibrary();
+})
+
+// Two below - calls the function for visibility between the form page for a new book and the info page for current books
+newBookBtn.addEventListener("click", () => {
+    toggle();
+})
+
+returnBtn.addEventListener("click", () => {
+    toggle();
 })
 
 // My library array
 let myLibrary = [];
 
 // Creating an object with a function in it to display info
-function Book(title, author, pages, complete) {
+Book = (title, author, pages, complete) => {
     this.title = title
     this.author = author 
     this.pages = pages
@@ -48,23 +58,31 @@ function Book(title, author, pages, complete) {
 }
 
 // Push the args into the library array
-function addToLibrary(obj) {
+addToLibrary = (obj) => {
     myLibrary.push(obj);
 }
 
 // Function to loop through array and display each book 
-function displayLibrary(){
+displayLibrary = () => {
     myLibrary.forEach(i => {
         console.table(i);
     });
 }
 
+// Wipes the values from each textbox for a new book
 clearForm = () => {
 
     bookTitle.value = null 
     bookAuthor.value = null 
     bookPages.value = null
     bookFinish.value = null
+}
+
+// Toggles visibility by adding/removing the "hidden" class attribute on these elements
+toggle = () => {
+    popUp.classList.toggle("hidden")
+    tableDisplay.classList.toggle("hidden")
+    newBookBtn.classList.toggle("hidden")
 }
 
 // Making new books to populate the library
